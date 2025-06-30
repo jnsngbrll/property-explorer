924b80d0f004033ff3387afccda5f76e7af2ef6e
Jansen Gabrillo <Gabrillojansen@gmail.com>
2025-06-30 21:10:26 +0800

    FEATURE: Add search bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { Text } from 'react-native';
 
 import { Container } from '@/components/container';
+import { SearchBar } from '@/components/search-bar';
 
 export default function Home() {
   return (
-    <Container>
+    <Container className="gap-y-4">
+      <SearchBar />
       <Text className="text-black dark:text-white">Home</Text>
     </Container>
   );
