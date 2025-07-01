62749f43762d60d065aaa7b89548336a4596f6fe
Jansen Gabrillo <Gabrillojansen@gmail.com>
2025-07-01 20:04:37 +0800

    CLEANUP: Remove unused files

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,12 @@
 import { Text } from 'react-native';
 
 import { Container } from '@/components/ui/container';
+import { Header } from '@/components/ui/header';
 
 export default function Profile() {
   return (
     <Container>
+      <Header title="Profile" />
       <Text className="text-black dark:text-white">Profile</Text>
     </Container>
   );
